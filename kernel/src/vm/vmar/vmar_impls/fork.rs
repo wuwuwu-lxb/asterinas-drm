lrh2000/asterinas

Add basic reverse mapping implementation

kernel/src/vm/vmar/vmar_impls/fork.rs
@@ -12,7 +12,7 @@ use ostd::{
     task::disable_preempt,
 };
 
-use super::{RssDelta, VMAR_CAP_ADDR, VMAR_LOWEST_ADDR, Vmar, VmarInner};
+use super::{RssDelta, Vmar, VmarInner};
 use crate::{prelude::*, process::ProcessVm};
 
 impl Vmar {
@@ -33,21 +33,28 @@ impl Vmar {
             let inner = vmar.inner.read();
             let mut new_inner = new_vmar.inner.write();
 
-            // Clone mappings.
-            let preempt_guard = disable_preempt();
-            let range = VMAR_LOWEST_ADDR..VMAR_CAP_ADDR;
-            let new_vmspace = new_vmar.vm_space();
-            let mut new_cursor = new_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
-            let cur_vmspace = vmar.vm_space();
-            let mut cur_cursor = cur_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
             let mut rss_delta = RssDelta::new(&new_vmar);
 
+            // Clone mappings.
             for vm_mapping in inner.vm_mappings.iter() {
                 let base = vm_mapping.map_to_addr();
+                let range = base..vm_mapping.map_end();
+
+                let mut rmap = vm_mapping.vmo().map(|vmo| vmo.vmo().rmap().lock());
 
                 // Clone the `VmMapping` to the new VMAR.
                 let new_mapping = vm_mapping.new_fork();
-                new_inner.insert_without_try_merge(new_mapping);
+                new_inner.insert_without_try_merge(
+                    vmar.vm_space(),
+                    new_mapping,
+                    rmap.as_deref_mut(),
+                );
+
+                let preempt_guard = disable_preempt();
+                let new_vmspace = new_vmar.vm_space();
+                let mut new_cursor = new_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
+                let cur_vmspace = vmar.vm_space();
+                let mut cur_cursor = cur_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
 
                 // Protect the mapping and copy to the new page table for COW.
                 cur_cursor.jump(base).unwrap();
@@ -56,12 +63,12 @@ impl Vmar {
                 let num_copied =
                     cow_copy_pt(&mut cur_cursor, &mut new_cursor, vm_mapping.map_size());
 
+                cur_cursor.flusher().issue_tlb_flush(TlbFlushOp::for_all());
+                cur_cursor.flusher().dispatch_tlb_flush();
+                cur_cursor.flusher().sync_tlb_flush();
+
                 rss_delta.add(vm_mapping.rss_type(), num_copied as isize);
             }
-
-            cur_cursor.flusher().issue_tlb_flush(TlbFlushOp::for_all());
-            cur_cursor.flusher().dispatch_tlb_flush();
-            cur_cursor.flusher().sync_tlb_flush();
         }
 
         Ok(new_vmar)

wuwuwu-lxb/asterinas-drm

kernel/src/vm/vmar/vmar_impls/fork.rs
@@ -12,7 +12,7 @@ use ostd::{
     task::disable_preempt,
 };
 
-use super::{RssDelta, VMAR_CAP_ADDR, VMAR_LOWEST_ADDR, Vmar, VmarInner};
+use super::{RssDelta, Vmar, VmarInner};
 use crate::{prelude::*, process::ProcessVm};
 
 impl Vmar {
@@ -33,21 +33,28 @@ impl Vmar {
             let inner = vmar.inner.read();
             let mut new_inner = new_vmar.inner.write();
 
-            // Clone mappings.
-            let preempt_guard = disable_preempt();
-            let range = VMAR_LOWEST_ADDR..VMAR_CAP_ADDR;
-            let new_vmspace = new_vmar.vm_space();
-            let mut new_cursor = new_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
-            let cur_vmspace = vmar.vm_space();
-            let mut cur_cursor = cur_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
             let mut rss_delta = RssDelta::new(&new_vmar);
 
+            // Clone mappings.
             for vm_mapping in inner.vm_mappings.iter() {
                 let base = vm_mapping.map_to_addr();
+                let range = base..vm_mapping.map_end();
+
+                let mut rmap = vm_mapping.vmo().map(|vmo| vmo.vmo().rmap().lock());
 
                 // Clone the `VmMapping` to the new VMAR.
                 let new_mapping = vm_mapping.new_fork();
-                new_inner.insert_without_try_merge(new_mapping);
+                new_inner.insert_without_try_merge(
+                    new_vmar.vm_space(),
+                    new_mapping,
+                    rmap.as_deref_mut(),
+                );
+
+                let preempt_guard = disable_preempt();
+                let new_vmspace = new_vmar.vm_space();
+                let mut new_cursor = new_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
+                let cur_vmspace = vmar.vm_space();
+                let mut cur_cursor = cur_vmspace.cursor_mut(&preempt_guard, &range).unwrap();
 
                 // Protect the mapping and copy to the new page table for COW.
                 cur_cursor.jump(base).unwrap();
@@ -56,12 +63,12 @@ impl Vmar {
                 let num_copied =
                     cow_copy_pt(&mut cur_cursor, &mut new_cursor, vm_mapping.map_size());
 
+                cur_cursor.flusher().issue_tlb_flush(TlbFlushOp::for_all());
+                cur_cursor.flusher().dispatch_tlb_flush();
+                cur_cursor.flusher().sync_tlb_flush();
+
                 rss_delta.add(vm_mapping.rss_type(), num_copied as isize);
             }
-
-            cur_cursor.flusher().issue_tlb_flush(TlbFlushOp::for_all());
-            cur_cursor.flusher().dispatch_tlb_flush();
-            cur_cursor.flusher().sync_tlb_flush();
         }
 
         Ok(new_vmar)